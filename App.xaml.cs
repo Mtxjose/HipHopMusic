53820364f73064a6edc6d341dc1a1fcbea4dfdd2
Mtxjose <161398265+Mtxjose@users.noreply.github.com>
2025-06-12 15:12:30 -0600

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HipHopMusic;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell(); // Inicia con navegación por Shell
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HipHopMusic.Views;
+namespace HipHopMusic;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+        Routing.RegisterRoute("canciones", typeof(CancionesPage));
+        Routing.RegisterRoute("detallecancion", typeof(DetalleCancionPage)); 
+
+       
+    }
+}
+
