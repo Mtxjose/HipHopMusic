7475b4c1a1132f24341705ffd7f3762c1cf8b9e3
Mtxjose <161398265+Mtxjose@users.noreply.github.com>
2025-06-12 15:25:23 -0600

    Add files via upload

---
+++ Models/Artista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HipHopMusic.Models
+{
+    public class Artista
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Imagen { get; set; } = string.Empty;
+        public string UltimoAlbum { get; set; } = string.Empty;
+        public string Ranking { get; set; } = string.Empty;  // ← CAMBIADO DE int A string
+    }
+}
---
+++ Models/Cancion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using HipHopMusic.Models;
+
+namespace HipHopMusic.Models
+{
+    public class Cancion
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Imagen { get; set; } = string.Empty;
+        public string Anio { get; set; } = string.Empty;
+        public string Duracion { get; set; } = string.Empty;
+        public string Letra { get; set; } = string.Empty;
+    }
+}
